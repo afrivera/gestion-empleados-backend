8709397f19fabb201affa8ad558d5eaed428043f
afrivera <andresrfelip@hotmail.com>
2022-10-01 17:13:24 -0500

    configuración de endpoint para obtener empleado por id y actualizar por id

--- src/main/java/com/gestion/empleados/controller/EmpleadoController.java
+++ src/main/java/com/gestion/empleados/controller/EmpleadoController.java
@@ -1,8 +1,10 @@
 package com.gestion.empleados.controller;
 
+import com.gestion.empleados.exceptions.ResourceNotFoundException;
 import com.gestion.empleados.model.Empleado;
 import com.gestion.empleados.repository.EmpleadoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -24,5 +26,26 @@ public class EmpleadoController {
     @PostMapping("/empleados")
     public Empleado guardarEmpleado(@RequestBody Empleado empleado){
         return repo.save(empleado);
+    }
+
+    // este metodo busca un empleado por Id
+    @GetMapping("/empleados/{id}")
+    public ResponseEntity<Empleado> obtenerEmpleadoPorId(@PathVariable Long id){
+        Empleado empleado = repo.findById(id)
+                .orElseThrow(()-> new ResourceNotFoundException("No Existe el empleado con el id: " + id));
+        return ResponseEntity.ok(empleado);
+    }
+
+    @PutMapping("/empleados/{id}")
+    public ResponseEntity<Empleado> actualizarEmpleado(@PathVariable Long id, @RequestBody Empleado detallesEmpleado){
+        Empleado empleado = repo.findById(id)
+                .orElseThrow(()-> new ResourceNotFoundException("No Existe el Empleado con el id: " + id ));
+        empleado.setNombre(detallesEmpleado.getNombre());
+        empleado.setApellido(detallesEmpleado.getApellido());
+        empleado.setEmail(detallesEmpleado.getEmail());
+
+        Empleado empleadoActualizado = repo.save(empleado);
+
+        return ResponseEntity.ok(empleadoActualizado);
     }
 }
